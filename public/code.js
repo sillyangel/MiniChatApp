b4bfd35b13bf6581954d16d41a3d766368f14de0
sillyangel <sillyangel3@gmail.com>
2023-01-26 03:09:36 +0000

    IM DONE YES

--- public/code.js
+++ public/code.js
@@ -32,6 +32,18 @@
         app.querySelector(".chat-screen #message-input").value = "";
     });
 
+    app.querySelector(".chat-screen #exit-chat").addEventListener("click",function(){
+        socket.emit("exituser",uname);
+        window.location.href = window.location.href;
+    });
+
+    socket.on("update", function(update){
+        renderMessage("update", update);
+    });
+    
+    socket.on("chat", function(message){
+        renderMessage("other", message);
+    });
     function renderMessage(type,message){
         let messageContainer = app.querySelector(".chat-screen #messages");
         if(type == "my"){
@@ -43,7 +55,24 @@
                     <div class="text">${message.text}</div>
                 </div>
             `;
+            messageContainer.appendChild(el);
+        } else if(type =="other") {
+            let el = document.createElement("div");
+            el.setAttribute("class", "message other-message");
+            el.innerHTML = `
+                <div>
+                    <div class="name">${message.username}</div>
+                    <div class="text">${message.text}</div>
+                </div>
+            `;
+            messageContainer.appendChild(el);
+        } else if(type == "update") {
+            let el = document.createElement("div");
+            el.setAttribute("class", "update");
+            el.innerText = message;
+            messageContainer.appendChild(el);
         }
+        messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
     }
 
 })();
