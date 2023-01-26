a1fbff77a6b00374202c0417427bc5f65b25110d
sillyangel <sillyangel3@gmail.com>
2023-01-26 03:27:07 +0000

    a

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const io = require("socket.io")(server);
 
 app.use(express.static(path.join(__dirname+"/public")));
 
-io.on("coonection", function (socket) {
-    socket.on("newuser",function(username){
-        socket.broadcast.emit("update", username + " joined the conversation");
-    });
-    socket.on("exituser",function(username){
-        socket.broadcast.emit("update", username + " left the conversation")
-    });
-    socket.on("chat",function(message){
-        socket.broadcast.emit("chat", message);
-    });
+io.on("connection", function(socket){
+	socket.on("newuser",function(username){
+		socket.broadcast.emit("update", username + " joined the conversation");
+	});
+	socket.on("exituser",function(username){
+		socket.broadcast.emit("update", username + " left the conversation");
+	});
+	socket.on("chat",function(message){
+		socket.broadcast.emit("chat", message);
+	});
 });
 
 server.listen(5000);
